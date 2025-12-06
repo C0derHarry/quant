dbac45088ed1abab232dc45f5aae0eee51d98e28
ball_lin <gillharsher1@gmail.com>
2025-12-06 12:03:44 +0530

    Update stock_utils.py
    updated functions to work globally

--- utils/stock_utils.py
+++ utils/stock_utils.py
@@ -2,23 +2,31 @@ import yfinance as yf
 import pandas as pd
 import datetime as dt
 
-def download_stock_data(stocks, days, interval="1d"):
+def download_stock_data(stocks, days, interval):
     """
-    Download stock close price data for given stocks & export to Excel.
+    Download OHLC stock data for given stocks & return as a dict.
 
     Args:
         stocks (list of str): List of stock symbols.
         days (int): Number of days back from today.
         interval (str): Data interval (e.g., "1d", "1wk", "1mo").
-        filename (str): Name of the output Excel file.
 
     Returns:
-        pd.DataFrame: DataFrame of downloaded stock data.
+        dict: {stock_symbol: DataFrame of OHLC data}
     """
-    start_date = dt.datetime.now() - dt.timedelta(days=days)
-    end_date = dt.datetime.now()
-    
-    data = yf.download(stocks, start=start_date, end=end_date, interval=interval)["Close"]
-    # data.to_excel(filename, index=True, engine="openpyxl")
-    # print(f"Data saved to {filename}")
-    return data
+    start_date = dt.datetime.today() - dt.timedelta(days=days)
+    end_date = dt.datetime.today()
+
+    # download data for all stocks at once
+
+    ohlc_data = {}
+
+    for stock in stocks:
+        try:
+            ohlc_data[stock] = yf.download(stock, start=start_date, end=end_date, interval=interval)
+            ohlc_data[stock].dropna(inplace=True, how="all")  # drop rows with all NaN values
+        except Exception as e:
+            print(f"Error downloading data for {stock}: {e}")
+            continue
+
+    return ohlc_data
